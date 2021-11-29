551de47f67ae222787a2ac1a995d9b7fe2c452f5
Wittline <8701464+Wittline@users.noreply.github.com>
2021-11-29 16:30:56 -0600

    Update scraping.py

--- app/scraping.py
+++ app/scraping.py
@@ -12,38 +12,38 @@ import re
 
 class scrapping():
 
-    def __init__(self, total):
-        self.total
-        self.min_pages = total//48
-        self.remainder =  total - (48 * self.min_pages)
-        if self.min_pages == 0:
-            self.remainder = 0
+    def __init__(self, total, size_page):
+        self.lowest  = True
+        self.total = total
+        self.size_page = size_page
+        self.min_pages = total//self.size_page
+        self.remainder = self.total
+        self.mc = []
+
+        if self.total >= self.size_page:
+            self.remainder =  self.total - (self.size_page * self.min_pages)
+            self.lowest = False
 
 
-    def __get_df_data(self, **kwargs):
-
-        mc = pd.DataFrame()
-
-        for k, v in kwargs.items():
-            mc[k] = v
-
-        return  mc
+    def __set_df_data(self, dd):
+        self.mc.append(dd)                
         
 
-    def __get_data_page(self, soup):
+    def __set_data_page(self, soup, remainder):
+
         first_pictures = soup.select('img.ui-search-result-image__element')
-        fp = [first_pictures[i]['data-src'] for i in range(0, len(first_pictures) - 1)]
+        fp = [first_pictures[i]['data-src'] for i in range(0, remainder)]
         property_names = soup.select('h2.ui-search-item__title')
-        pn = [property_names[i].text for i in range(0, len(property_names) - 1)]
+        pn = [property_names[i].text for i in range(0, remainder)]
         urls = soup.select('a.ui-search-result__content')
-        url = [urls[i]['href'] for i in range(0, len(urls) - 1)]
+        url = [urls[i]['href'] for i in range(0, remainder)]
         prices = soup.select('span.price-tag-text-sr-only')
-        prc = [prices[i].text for i in range(0, len(prices) - 1)]
+        prc = [prices[i].text for i in range(0, remainder)]
         addresses = soup.select('span.ui-search-item__location')
-        adr = [addresses[i].text for i in range(0, len(addresses) - 1)]
+        adr = [addresses[i].text for i in range(0, remainder )]
         sizes = soup.select('li.ui-search-card-attributes__attribute')
-        siz = [re.findall("\d+", sizes[i].text)[0] for i in range(0, len(sizes) - 1) if i % 2 == 0]
-        ame = [sizes[i].text for i in range(0, len(sizes) - 1) if i % 2 != 0]
+        siz = [re.findall("\d+", sizes[i].text)[0] for i in range(0, (remainder*2)) if i % 2 == 0]
+        ame = [sizes[i].text for i in range(0, (remainder*2)) if i % 2 != 0]
         desc = []
         for ur in url:
             descriptions_response  = requests.get(ur)
@@ -52,40 +52,50 @@ class scrapping():
             desc.append(descriptions[0].text)
 
         
-        mc = self.__get_df_data(property_name = pn, url = url, 
-                    price = prc, adress = adr, 
-                    street = adr, number = adr, 
-                    settlement = adr, town = adr, 
-                    state = adr, county = adr, description = desc,
-                    amenities = ame, size = siz, first_picture = fp)
-        
-        return mc
+        dict_data = {
+                    'property_name': pn, 
+                     'url' : url, 
+                     'price' : prc, 
+                     'adress' : adr, 
+                     'street' : adr, 
+                     'number' : adr, 
+                     'settlement' : adr, 
+                     'town' : adr, 
+                     'state' : adr, 
+                     'county' : adr, 
+                     'description' : desc, 
+                     'amenities' : ame,
+                     'size' : siz, 
+                     'first_picture' : fp
+                     }
+
+        self.__set_df_data(dict_data)
+                
+
+    def __get_soup(self, n):
+            URL = 'https://inmuebles.metroscubicos.com/casas/venta/quintana-roo/_Desde_{number}_NoIndex_True'.format(number=n)
+            response = requests.get(URL)
+            return BeautifulSoup(response.content, 'lxml')
 
 
-
-    def get_data(self, total):
-
-        mc = self.__get_df_data(property_name = None, url = None, 
-            price = None, adress = None, 
-            street = None, number = None, 
-            settlement = None, town = None, 
-            state = None, county = None, description = None,
-            amenities = None, size = None, first_picture = None)
+    def get_data(self):
 
         c = 0
-        for p in range(0, self.min_pages, 1):
-            if p > 0:
-                c = (p * 48) + 1
-            URL = 'https://inmuebles.metroscubicos.com/casas/venta/quintana-roo/_Desde_{number}_NoIndex_True'.format(number=c)
-            response = requests.get(URL)
-            soup = BeautifulSoup(response.content, 'lxml')
-            mc.append(self.__get_data_page(soup, self, total, 0))
+        if self.lowest:
+            soup = self.__get_soup(0)
+            self.__set_data_page(soup, self.remainder)
+        else:
+            for p in range(0, self.min_pages, 1):
+                print("pagina:", p)
+                if p > 0:
+                    c = (p * self.size_page) + 1
+                soup = self.__get_soup(c)
+                self.__set_data_page(soup, self.size_page)
 
-
-        if self.remainder > 0:
-            c += 48
-            URL = 'https://inmuebles.metroscubicos.com/casas/venta/quintana-roo/_Desde_{number}_NoIndex_True'.format(number=c + 1)            
-            response = requests.get(URL)
-            soup = BeautifulSoup(response.content, 'lxml')
-            li_elements = soup.select('li.ui-search-layout__item')
-            for li in range(0, remainder, total,1):
+            if self.remainder > 0:
+                c += self.size_page
+                soup = self.__get_soup(c + 1)                
+                self.__set_data_page(soup, self.remainder)
+ 
+              
+        return pd.concat([pd.DataFrame(self.mc[i], columns=self.mc[i].keys()) for i in range(0, len(self.mc))], axis =0).reset_index()
